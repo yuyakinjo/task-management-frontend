ade49bd026a3202db9ecf1e8eed8d5a48c5d1fde
yuyakinjo <yuyakinjo@users.noreply.github.com>
2021-08-06 08:18:28 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.c8c1a1b5bbb08de4b664a413cfdc1d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf58c633ba2d6577cf789f8e24736a4",
+    "url": "https://yuyakinjo-io.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "c213e89acde93f499482",
+    "url": "https://yuyakinjo-io.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e93b7873e4e6f3da45bb",
+    "url": "https://yuyakinjo-io.github.io/task-management-frontend/static/js/2.5cfd42eb.chunk.js"
+  },
+  {
+    "revision": "c213e89acde93f499482",
+    "url": "https://yuyakinjo-io.github.io/task-management-frontend/static/js/main.096c994f.chunk.js"
+  },
+  {
+    "revision": "3301a641608a53fd0827",
+    "url": "https://yuyakinjo-io.github.io/task-management-frontend/static/js/runtime~main.0df5cbed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://yuyakinjo-io.github.io/task-management-frontend/precache-manifest.c8c1a1b5bbb08de4b664a413cfdc1d6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://yuyakinjo-io.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
